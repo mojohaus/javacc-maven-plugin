a2ef97a0f82046b30cdfe0c6df2ee46c30bfb04f
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2008-02-03 21:32:31 +0000

    o Added some more unit tests
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javacc-maven-plugin@6198 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/test/java/org/codehaus/mojo/javacc/ForkedJvmTest.java
@@ -0,0 +1,96 @@
+package org.codehaus.mojo.javacc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file 
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied.  See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
+
+import java.io.File;
+
+import org.codehaus.plexus.util.cli.CommandLineUtils.StringStreamConsumer;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests <code>ForkedJvm</code>.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class ForkedJvmTest
+    extends TestCase
+{
+
+    public void testToStringNullSafe()
+        throws Exception
+    {
+        ForkedJvm jvm = new ForkedJvm();
+        String string = jvm.toString();
+        assertNotNull( string );
+        assertTrue( string.indexOf( "null" ) < 0 );
+    }
+
+    public void testSettersNullSafe()
+        throws Exception
+    {
+        ForkedJvm jvm = new ForkedJvm();
+        jvm.addArgument( (File) null );
+        jvm.addArgument( (String) null );
+        jvm.addArguments( null );
+        jvm.addArguments( new String[] { null } );
+        jvm.addClassPathEntry( (Class) null );
+        jvm.addClassPathEntry( (File) null );
+        jvm.addClassPathEntry( (String) null );
+        jvm.setMainClass( (Class) null );
+        jvm.setMainClass( (String) null );
+        jvm.setSystemOut( null );
+        jvm.setSystemErr( null );
+    }
+
+    public void testFork()
+        throws Exception
+    {
+        File file = new File( "test" ).getAbsoluteFile();
+        String nonce = Integer.toString( hashCode() );
+
+        StringStreamConsumer stdout = new StringStreamConsumer();
+        StringStreamConsumer stderr = new StringStreamConsumer();
+
+        ForkedJvm jvm = new ForkedJvm();
+        jvm.setSystemOut( stdout );
+        jvm.setSystemErr( stderr );
+        jvm.setMainClass( MainStub.class );
+        jvm.addArgument( nonce );
+        jvm.addArguments( new String[] { "arg1", "arg2" } );
+        jvm.addArgument( file );
+        int exitcode = jvm.run();
+        String out = stdout.getOutput();
+        String err = stderr.getOutput();
+        String[] args = out.split( "(\r\n)|(\r)|(\n)" );
+
+        assertEquals( 27, exitcode );
+
+        assertEquals( "stderr", err.trim() );
+
+        assertEquals( 4, args.length );
+        assertEquals( nonce, args[0] );
+        assertEquals( "arg1", args[1] );
+        assertEquals( "arg2", args[2] );
+        assertEquals( file.getPath(), args[3] );
+    }
+
+}
---
+++ src/test/java/org/codehaus/mojo/javacc/JJTreeTest.java
@@ -0,0 +1,64 @@
+package org.codehaus.mojo.javacc;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file 
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied.  See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
+
+import junit.framework.TestCase;
+
+/**
+ * Tests <code>JJTree</code> facade.
+ * 
+ * @author Benjamin Bentmann
+ * @version $Id$
+ */
+public class JJTreeTest
+    extends TestCase
+{
+
+    public void testToStringNullSafe()
+        throws Exception
+    {
+        JJTree tool = new JJTree();
+        String string = tool.toString();
+        assertNotNull( string );
+        assertTrue( string.indexOf( "null" ) < 0 );
+    }
+
+    public void testSettersNullSafe()
+        throws Exception
+    {
+        JJTree tool = new JJTree();
+        tool.setInputFile( null );
+        tool.setOutputDirectory( null );
+        tool.setJdkVersion( null );
+        tool.setStatic( null );
+        tool.setBuildNodeFiles( null );
+        tool.setMulti( null );
+        tool.setNodeDefaultVoid( null );
+        tool.setNodeFactory( null );
+        tool.setNodePackage( null );
+        tool.setNodePrefix( null );
+        tool.setNodeScopeHook( null );
+        tool.setNodeUsesParser( null );
+        tool.setVisitor( null );
+        tool.setVisitorException( null );
+        tool.setLog( null );
+    }
+
+}
