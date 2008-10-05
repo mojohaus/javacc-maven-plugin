3ad6c3ad229ad451600f4fe72a21523923af021c
Benjamin Bentmann <benjamin.bentmann@udo.edu>
2008-10-05 14:55:48 +0000

    o Updated ITs to also work with JavaCC 4.1
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/javacc-maven-plugin@7841 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/it/javacc-custom-parser-files/src/main/javacc/org/codehaus/javacc/simple/Token.java
+++ src/it/javacc-custom-parser-files/src/main/javacc/org/codehaus/javacc/simple/Token.java
@@ -1,29 +1,55 @@
 /* CUSTOMIZED PARSER FILE - MUST NOT BE OVERWRITTEN BY JAVACC */
 package org.codehaus.javacc.simple;
 
-public class Token {
+public class Token
+{
 
-  public int kind;
+    public int kind;
 
-  public int beginLine, beginColumn, endLine, endColumn;
+    public int beginLine, beginColumn, endLine, endColumn;
 
-  public String image;
+    public String image;
 
-  public Token next;
+    public Token next;
 
-  public Token specialToken;
+    public Token specialToken;
 
-  public String toString()
-  {
-     return image;
-  }
+    public Token()
+    {
+    }
 
-  public static final Token newToken(int ofKind)
-  {
-     switch(ofKind)
-     {
-       default : return new Token();
-     }
-  }
+    public Token( int kind )
+    {
+       this( kind, null );
+    }
+
+    public Token( int kind, String image )
+    {
+       this.kind = kind;
+       this.image = image;
+    }
+
+    public Object getValue()
+    {
+        return null;
+    }
+
+    public String toString()
+    {
+       return image;
+    }
+
+    public static Token newToken( int ofKind, String image )
+    {
+        switch ( ofKind )
+        {
+            default : return new Token( ofKind, image );
+        }
+    }
+
+    public static Token newToken( int ofKind )
+    {
+       return newToken( ofKind, null );
+    }
 
 }
